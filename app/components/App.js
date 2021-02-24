81b31faf842515f03ce635d0d5fd7305e9d32b48
Martin Fencl <martin@layeredcode.com>
2021-02-24 16:26:12 -0600

    clean up App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,6 @@
 // Adapted from: https://btholt.github.io/complete-intro-to-react-v1/
 
 var React = require('react');
-// var ReactDOM = require('react-dom'); // <= (not) needed?
 
 class App extends React.Component {
   render() {
@@ -14,16 +13,3 @@ class App extends React.Component {
 }
 
 module.exports = App;
-
-// New(er) way?
-// With no require needed, maybe?
-
-// function App() {
-//   return (
-//     <div className='App'>
-//       <p>Hello from the app I guess?</p>
-//     </div>
-//   );
-// }
-
-// export default App;
